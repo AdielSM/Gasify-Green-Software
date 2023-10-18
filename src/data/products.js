d5af940648dbcc5ed02eeaeffea347347d20c2c3
AdielSM <129189553+AdielSM@users.noreply.github.com>
2023-10-17 21:10:10 -0300

    Algumas alterações

---
+++ src/data/products.js
@@ -0,0 +1,21 @@
+export const products = [
+    {
+        id: '3b7860f0-91f7-4c54-a2db-eb34cdeb02b7',
+        name: 'Gasolina Comum',
+        price: 569,
+        cc_for_unit: .2 
+    },
+    {
+        id: '6921ea3a-1f91-44cc-84b9-f61957284fe8',
+        name: 'Gasolina Aditivada',
+        price: 569,
+        cc_for_unit: .2 
+    },
+    {
+        id: 'fc6ba16e-43d0-409e-adc5-aed0512324a5',
+        name: 'Etanol',
+        price: 399,
+        cc_for_unit: 1
+    }
+]
+
