401ccee38f17dc22fdb07a0623a1df1013a4df12
AdielSM <129189553+AdielSM@users.noreply.github.com>
2023-10-24 21:09:58 -0300

    Adicionando dist para deploy

--- src/models/itemCarrinho.js
+++
@@ -1,8 +0,0 @@
-export default class itemCarrinho {
-  constructor(id, quantidade, valor, cc_for_unit) {
-    this.id = id;
-    this.quantidade = quantidade;
-    this.valor = valor;
-    this.cc_for_unit = cc_for_unit;
-  }
-}
--- src/utils.js
+++
@@ -1,3 +0,0 @@
-export function formatNumberPer100(number) {
-  return number / 100;
-}
